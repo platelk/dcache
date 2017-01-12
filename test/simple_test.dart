08aac9126625ed281e734593d9828867ddb11df5
PLATEL Kevin <platel.kevin@gmail.com>
2017-01-12 13:54:03 +0100

    Adding simple test set/get

--- test/simple_test.dart
+++ test/simple_test.dart
@@ -4,7 +4,14 @@ import 'package:dcache/dcache.dart';
 
 void main() {
   test("Test cache initialization", () {
-    var cache = new SimpleCache();
-    expect(cache, isNotNull)
+    Cache cache = new SimpleCache();
+    expect(cache, isNotNull);
+  });
+  //
+  test("Test simple insert/get", () {
+    Cache c = new SimpleCache();
+
+    c.set("key", 42);
+    expect(c.get("key"), equals(42));
   });
 }
